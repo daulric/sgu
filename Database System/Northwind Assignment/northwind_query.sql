759261091bf349a03307358d7a6953decb3a76bc
Ulric <ulricaird1@gmail.com>
2026-09-25 10:32:07 -0400

    Update northwind_query.sql

--- Database System/Northwind Assignment/northwind_query.sql
+++ Database System/Northwind Assignment/northwind_query.sql
@@ -6,14 +6,16 @@ WHERE `Country` IS NOT NULL
 GROUP BY `Country`;
 
 -- 2. Provide a list of all such products with high quantities in stock, i.e., 50 or more.
-SELECT `ProductName` 
-FROM Products
-WHERE `UnitsInStock` >= 50;
+SELECT `Products`.`ProductName`
+FROM `Products`
+INNER JOIN `Categories` 
+ON `Products`.`CategoryID` = `Categories`.`CategoryID`
+WHERE `Products`.`UnitsInStock` >= 50 AND `Categories`.`CategoryName` IN ('Beverages', 'Condiments', 'Confections');
 
 -- 3. Which active products are at or below the minimum stock level (Hint: ReorderLevel) and have not been reordered yet?
 SELECT `ProductName` 
 FROM `Products`
-WHERE `UnitsInStock` < `ReorderLevel`;
+WHERE `UnitsInStock` <= `ReorderLevel` AND `Discontinued` = 0 AND `UnitsOnOrder` = 0;
 
 -- 4. Return the Product ID, Product Name and Price of products who name starts with ‘C’ and the price is between 18 and 22
 SELECT `ProductID`, `ProductName`, `UnitPrice` 
@@ -33,16 +35,18 @@ JOIN `Region` ON `Territories`.`RegionID` = `Region`.`RegionID`
 GROUP BY `Region`.`RegionDescription` ,`Region`.`RegionID`;
 
 -- 7. Are there any territories for which there are no employees assigned? If so, list the names (only) of such territories and the corresponding region. Note: Use joins to accomplish this
-SELECT `TerritoryDescription`
+SELECT `TerritoryDescription`, `Region`.`RegionDescription`
 FROM `Territories`
 LEFT JOIN `EmployeeTerritories` ON `Territories`.`TerritoryID` = `EmployeeTerritories`.`TerritoryID`
+INNER JOIN `Region` ON `Territories`.`RegionID` = `Region`.`RegionID`
 WHERE EmployeeTerritories.TerritoryID IS NULL;
 
 -- 8. Show total number of products per category. Only include categories with at least 2 products.
 SELECT `Categories`.`CategoryName`, COUNT(`Products`.`ProductID`) as TotalProducts
 FROM `Products`
 INNER JOIN `Categories` ON `Products`.`CategoryID` = `Categories`.`CategoryID`
-GROUP BY `Categories`.`CategoryID`, `Categories`.`CategoryName`;
+GROUP BY `Categories`.`CategoryID`, `Categories`.`CategoryName`
+HAVING COUNT(`Products`.`ProductID`) >= 2;
 
 -- 9. Which shipper shipped the most orders? List the shipper name and the amount of orders shipped. Your query should return only the shipper that meets this requirement. Sorting in descending order does not suffice
 SELECT `Shippers`.`CompanyName` AS `ShipperName`, COUNT(`Orders`.`OrderID`) AS `TotalOrders`
